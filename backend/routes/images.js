095bd74164797c0e2a865c0252451ad337acb5a9
Belal <belalk@live.ca>
2023-03-17 00:30:58 -0700

    fix backend

--- backend/routes/images.js
+++ backend/routes/images.js
@@ -1,8 +1,28 @@
 const express = require("express")
 const router = express.Router()
+const multer = require('multer')
 
-router.get("/", (req, res) => {
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, '../frontend/public/images')
+  },
+  filename: function (req, file, cb) {
+    // Generate a unique filename with the original extension
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    const extension = file.originalname.split('.').pop()
+    cb(null, file.fieldname + '-' + uniqueSuffix + '.' + extension)
+  }
+})
+
+const upload = multer({ storage: storage });
+
+
+router.get("/",  (req, res) => {
     res.send("images")
+})
+
+router.post("/", upload.single('image'), (req, res) => {
+    return res.send(req.file.filename)
 })
 
 module.exports = router
