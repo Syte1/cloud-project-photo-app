af87a5d4f5cebffecac588989dc97a63c10ff628
Belal <belalk@live.ca>
2023-03-18 06:16:23 -0700

    fix refresh bug

--- backend/routes/images.js
+++ backend/routes/images.js
@@ -1,10 +1,11 @@
 const express = require("express")
 const router = express.Router()
 const multer = require('multer')
+const path = require("path");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, '../frontend/public/images')
+    cb(null, 'images')
   },
   filename: function (req, file, cb) {
     // Generate a unique filename with the original extension
@@ -16,13 +17,16 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
+router.use(express.static("images"));
 
-router.get("/",  (req, res) => {
-    res.send("images")
-})
+router.get("/:imageName", (req, res) => {
+  const imageName = req.params.imageName;
+  const imagePath = path.join(__dirname, "images", imageName);
+  res.sendFile(imagePath);
+});
 
 router.post("/", upload.single('image'), (req, res) => {
-    return res.send(req.file.filename)
+    return res.send(JSON.stringify(req.file))
 })
 
 module.exports = router
