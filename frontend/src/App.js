10ffbfc58d5c95a9af4ab36f3d0b9280e9c610d4
Belal <belalk@live.ca>
2023-03-18 06:16:44 -0700

    use fetch instead

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,28 @@
 import { useState } from 'react'
 import UploadBar from './components/UploadBar'
 import ImageGallery from './components/ImageGallery'
-import axios from 'axios'
 
 function App() {
 
     const [images, setImages] = useState([])
 
-    const handleSubmit = (image1) => {
+    const handleSubmit = async (image1) => {
         postImage(image1)
         
     }
-    const postImage = async (image2) => {
-        
+    const postImage = (image2) => {
         const formData = new FormData()
         formData.append('image', image2)
-        const response = await axios.post('http://localhost:3001/images', formData)
-        setImages([response.data, ...images])
+        const requestOptions = {
+            method: 'POST',
+            body: formData
+        }
+        fetch('http://localhost:3001/images', requestOptions)
+            .then(response => response.json())
+            .then(data=> {
+                console.log(data.filename)
+                setImages([...images, data.filename])
+            })
     }
     return (
         <div>
