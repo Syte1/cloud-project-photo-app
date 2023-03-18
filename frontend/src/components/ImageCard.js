337682de89959967fbda67132ade89285d923bbc
Belal <belalk@live.ca>
2023-03-18 06:16:51 -0700

    display images

--- frontend/src/components/ImageCard.js
+++ frontend/src/components/ImageCard.js
@@ -1,9 +1,7 @@
 function ImageCard({image}) {
-    const filename = "./images/" + image
-    console.log(filename)
     return (
         <div>
-            <img src={filename} alt="s"></img>
+            <img src={`http://localhost:3001/images/${image}`} alt={image}></img>
         </div>
     )
 }
